8495297a6fa9d7bf17b26c413ef64a49b7541d9e
Raphael Forment <raphael.forment@gmail.com>
2023-06-26 01:08:22 +0200

    experimenting more with canvas

--- index.js
+++ index.js
@@ -4,27 +4,9 @@ const page = {
   canvas: document.getElementById("canvas"),
   scale: window.devicePixelRatio,
   context: canvas.getContext("2d"),
-  width: Math.floor(window.innerWidth * window.devicePixelRatio || 2),
+  width: Math.floor(window.innerWidth  * window.devicePixelRatio || 2),
   height: Math.floor(window.innerHeight * window.devicePixelRatio || 2),
 };
-
-function handleResize() {
-  page.scale = window.devicePixelRatio;
-  page.width = Math.floor(window.innerWidth * window.devicePixelRatio || 2);
-  page.height = Math.floor(window.innerHeight * window.devicePixelRatio || 2);
-  
-  page.canvas.width = page.width;
-  page.canvas.height = page.height;
-  
-  page.context.scale(page.scale, page.scale);
-  page.context.canvas.width = page.width;
-  page.context.canvas.height = page.height;
-  
-  console.log("Resizing like a pro");
-}
-
-window.onresize = handleResize;
-handleResize();
 
 // Read tables data from Local Storage
 let saved_tables = localStorage.getItem("tables");
